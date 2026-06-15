3a1bfc254055d2fa695091e3b55667fbe74391d6
StevNath <stevennmeliala@gmail.com>
2026-06-15 13:19:10 +0700

    Update transaction.jsx

--- frontend/src/pages/Transaction/transaction.jsx
+++ frontend/src/pages/Transaction/transaction.jsx
@@ -4,6 +4,7 @@ import Table from "../../components/Table.jsx";
 
 export default function Transaction() {
   const [transactions, setTransactions] = useState([]);
+  const [searchId, setSearchId] = useState("");
 
   const columns = [
     { header: "ID", accessor: "id_transaction" },
@@ -16,16 +17,18 @@ export default function Transaction() {
   ];
 
   function formatDateWIB(dateString) {
-    return new Date(dateString).toLocaleString("id-ID", {
-      timeZone: "Asia/Jakarta",
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false,
-    }) + " WIB";
+    return (
+      new Date(dateString).toLocaleString("id-ID", {
+        timeZone: "Asia/Jakarta",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      }) + " WIB"
+    );
   }
 
   async function fetchTransactions() {
@@ -44,9 +47,35 @@ export default function Transaction() {
       }));
 
       setTransactions(formattedData);
-
     } catch (err) {
       console.error(err.response?.data || err.message);
+    }
+  }
+
+  async function handleSearch() {
+    try {
+      if (!searchId) {
+        fetchTransactions();
+        return;
+      }
+
+      const token = localStorage.getItem("token");
+
+      const res = await api.get(`/transactions/${searchId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setTransactions([
+        {
+          ...res.data,
+          tanggal_wib: formatDateWIB(res.data.tanggal),
+        },
+      ]);
+    } catch (err) {
+      alert("Transaksi tidak ditemukan");
+      setTransactions([]);
     }
   }
 
@@ -59,6 +88,33 @@ export default function Transaction() {
       <h1 className="text-2xl font-bold mb-6">
         Log Transaksi
       </h1>
+
+      <div className="flex gap-2 mb-4">
+        <input
+          type="number"
+          placeholder="Cari ID Transaksi"
+          value={searchId}
+          onChange={(e) => setSearchId(e.target.value)}
+          className="border border-gray-300 rounded px-4 py-2"
+        />
+
+        <button
+          onClick={handleSearch}
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer"
+        >
+          Cari
+        </button>
+
+        <button
+          onClick={() => {
+            setSearchId("");
+            fetchTransactions();
+          }}
+          className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 cursor-pointer"
+        >
+          Reset
+        </button>
+      </div>
 
       <Table
         columns={columns}
